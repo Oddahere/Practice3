44735fdc167d8a447d558e3aa6f4d3e4d849f14e
Oddahere <66585882+Oddahere@users.noreply.github.com>
2020-06-08 00:28:22 -0500

    Create main.py
    Homework #1

---
+++ main.py
@@ -0,0 +1,22 @@
+print("Birthday Calculator")
+print("Current day")
+month = int(input("Month: "))
+day = int(input("Day: "))
+year = int(input("Year: "))
+
+print("Birthday")
+month_birthday = int(input("Month: "))
+day_birthday = int(input("Day: "))
+year_birthday = int(input("Year: "))
+
+
+age = 0;
+age = (year-year_birthday-1)
+if(month >= month_birthday):
+  if(month > month_birthday):
+    age = age+1
+  elif(day >= day_birthday):
+    age = age+1
+  
+
+print("You are", age, "years old.")
